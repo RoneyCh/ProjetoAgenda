abc78e7f3efe27bbf45d7a85b990a64e8ceb102a
RoneyCh <roneychristian159@gmail.com>
2022-03-27 23:43:49 -0300

    iniciando validacao

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const LoginSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descricao: String
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
+  
+  register() {
+    this.isValid();
+    if(this.errors.length > 0) return;
+  }
+
+  isValid() {
+    // Validation
+    this.cleanUp();
+    // Email validation
+    if(!validator.isEmail(this.body.email)) this.errors.push('Email inválido');
+    // Password Validation
+    if(this.body.password.length < 4 || this.body.password > 12) this.errors.push('A senha precisa ter entre 4 e 12 caracteres');
+
+  }
+
+  cleanUp() {
+    for(const key in this.body) {
+      if(typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
+    }
+  
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    };
+  }
+
+
 
 }
 
